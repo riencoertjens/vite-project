6433526b5e724bf680f3648011f61097fb2a893c
Rien Coertjens <rien.coertjens@gmail.com>
2023-12-05 11:20:17 +0100

    try again

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test";
 import { argosScreenshot } from "@argos-ci/playwright";
 
 test("has title", async ({ page }) => {
-  await page.goto("http://localhost:4173/");
+  await page.goto("/");
   await argosScreenshot(page, "homepage");
 
   await expect(
